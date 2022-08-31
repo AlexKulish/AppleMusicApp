5d0e24ad0d24f2b6274af51a3daf014c497b1f0c
Alexey Kulish <alexkulish1993@gmail.com>
2022-08-31 15:29:55 +0300

    Initial Commit

---
+++ AppleMusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleMusicApp
+//
+//  Created by Alex Kulish on 31.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
