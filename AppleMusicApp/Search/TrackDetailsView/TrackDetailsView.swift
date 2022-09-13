41fc7d8907c01b4adc7d373880bc951f6706f86e
Alexey Kulish <alexkulish1993@gmail.com>
2022-09-13 14:11:43 +0300

    Setup UI elements in TrackDetailsView

--- AppleMusicApp/Search/TrackDetailsView/TrackDetailsView.swift
+++ AppleMusicApp/Search/TrackDetailsView/TrackDetailsView.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class TrackDetailsView: UIView {
     
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var currentTimeSlider: UISlider!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var trackNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
     override class func awakeFromNib() {
         super.awakeFromNib()
         
+    }
+    
+    @IBAction func handleCurrentTimeSlider(_ sender: Any) {
+    }
+    
+    @IBAction func handleVolumeSlider(_ sender: Any) {
+    }
+    
+    @IBAction func hideScreenButtonPressed(_ sender: Any) {
+        self.removeFromSuperview()
+    }
+    
+    @IBAction func previousTrackButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextTrackButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func playPauseButtonPressed(_ sender: Any) {
     }
     
 }
