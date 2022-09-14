57912cdb7827115e217f6e43b02b2c838cd0de59
Alexey Kulish <alexkulish1993@gmail.com>
2022-09-14 19:06:39 +0300

    Add animations for TrackImageView. Add TimeObserver where track begin play

--- AppleMusicApp/Search/TrackDetailsView/TrackDetailsView.swift
+++ AppleMusicApp/Search/TrackDetailsView/TrackDetailsView.swift
@@ -11,6 +11,8 @@ import AVKit
 
 class TrackDetailsView: UIView {
     
+    // MARK: - IBOutlets
+    
     @IBOutlet weak var trackImageView: UIImageView!
     @IBOutlet weak var currentTimeSlider: UISlider!
     @IBOutlet weak var currentTimeLabel: UILabel!
@@ -20,16 +22,25 @@ class TrackDetailsView: UIView {
     @IBOutlet weak var playPauseButton: UIButton!
     @IBOutlet weak var volumeSlider: UISlider!
     
+    // MARK: - Private properties
+    
     private lazy var player: AVPlayer = {
         let player = AVPlayer()
         player.automaticallyWaitsToMinimizeStalling = false
         return player
     }()
     
-    override class func awakeFromNib() {
+    // MARK: - awakeFromNib
+    
+    override func awakeFromNib() {
         super.awakeFromNib()
         
+        let scale: CGFloat = 0.8
+        trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        trackImageView.layer.cornerRadius = 10
     }
+    
+    // MARK: - IBActions
     
     @IBAction func handleCurrentTimeSlider(_ sender: Any) {
     }
@@ -52,12 +63,16 @@ class TrackDetailsView: UIView {
         if player.timeControlStatus == .paused {
             player.play()
             playPauseButton.setImage(UIImage(named: "Pause"), for: .normal)
+            increaseTrackImageView()
         } else {
             player.pause()
             playPauseButton.setImage(UIImage(named: "play"), for: .normal)
+            decreaseTrackImageView()
         }
         
     }
+    
+    // MARK: - Setup
     
     func setupFrame(with someView: UIView) {
         self.frame = someView.frame
@@ -67,6 +82,7 @@ class TrackDetailsView: UIView {
         trackNameLabel.text = viewModel.trackName
         artistNameLabel.text = viewModel.artistName
         playTrack(with: viewModel.previewUrl)
+        monitoringStartTime()
         let urlString600 = viewModel.iconStringUrl?.replacingOccurrences(of: "100x100", with: "600x600") ?? ""
         guard let url = URL(string: urlString600) else { return }
         trackImageView.sd_setImage(with: url, completed: nil)
@@ -77,6 +93,36 @@ class TrackDetailsView: UIView {
         let playerItem = AVPlayerItem(url: url)
         player.replaceCurrentItem(with: playerItem)
         player.play()
+    }
+    
+    // MARK: - Animations
+    
+    private func increaseTrackImageView() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+            self.trackImageView.transform = .identity
+        }, completion: nil)
+    }
+    
+    private func decreaseTrackImageView() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+            let scale: CGFloat = 0.8
+            self.trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        }, completion: nil)
+    }
+    
+    // MARK: - Time observer
+    
+    private func monitoringStartTime() {
+        let time = CMTimeMake(value: 1, timescale: 3)
+        let times = [NSValue(time: time)]
+        
+        player.addBoundaryTimeObserver(forTimes: times, queue: .main) { [weak self] in
+            self?.increaseTrackImageView()
+        }
+    }
+    
+    deinit {
+        print("deinit class")
     }
     
 }
